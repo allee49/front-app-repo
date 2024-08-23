99570ff5f97ccabded89f0094472a4886a5525eb
allee49 <lewntk49@gmail.com>
2024-08-23 08:57:30 +0000

    kustomize4

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            Have a nice weekend
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
